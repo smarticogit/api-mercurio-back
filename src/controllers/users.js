cc732bf347b97353a2e6e28862702b969361e3e2
Daniel <daniel.repres@hotmail.com>
2023-08-03 21:47:14 -0300

    Refactor Sql Injection

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,70 +1,96 @@
-const users = require('../database/data');
-const pool = require('../connection');
+const users = require("../database/data");
+const pool = require("../connection");
 
 const usersList = async (req, res) => {
-    try {
-        const usersList = await pool.query('select * from uses');
-        res.status(200).send(usersList.rows);
-    } catch (error) {
-        res.status(500).send(error.message);
+  try {
+    const query = "select * from users";
+    const usersList = await pool.query(query);
+    return res.status(200).send(usersList.rows);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+const userGet = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = `select * from users where id = $1`;
+    const params = [Number(id)];
+
+    const userFound = await pool.query(query, params);
+    if (userFound.rowCount === 0) {
+      return res.status(404).send({ message: "User not found" });
     }
-}
+    res.status(200).send(userFound.rows[0]);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
 
-const userGet = (req, res) => {
-    const { id } = req.params;
+const userCreate = async (req, res) => {
+  const data = req.body;
 
-    if(Number(id) > users.length) {
-        res.send("Invalid ID")
-    } else {
-        const user = users.find( user => user.id === Number(id));
-        res.status(200).send(user)
-    }
-}
-
-const userCreate = (req, res) => {
-    const data = req.body;
-
+  try {
     const newUser = {
-        id: users.length + 1,
-        name: data.name,
-        email: data.email,
-        password: data.password
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+    const queryEmail = "select * from users where email = $1";
+    const paramsEmail = [newUser.email];
+
+    const emailExists = await pool.query(queryEmail, paramsEmail);
+
+    if (emailExists.rowCount > 0) {
+      return res.status(404).send({ message: "Email already exists" });
     }
 
-    users.push(newUser);
+    const query =
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)";
+
+    const params = [newUser.name, newUser.email, newUser.password];
+
+    const result = await pool.query(query, params);
+
+    if (result.rowCount === 0) {
+      return res.status(404).send({ message: "User not created" });
+    }
     res.status(201).send(newUser);
-}
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
 
 const userUpdate = (req, res) => {
-    const { id } = req.params;
-    const { name, email, password } = req.body;
+  const { id } = req.params;
+  const { name, email, password } = req.body;
 
-    if(Number(id) > users.length) {
-        res.send("Invalid ID")
-    } else {
-        const user = users.find( user => user.id === Number(id));
+  if (Number(id) > users.length) {
+    res.send("Invalid ID");
+  } else {
+    const user = users.find((user) => user.id === Number(id));
 
-        user.name = name;
-        user.email = email;
-        user.password = password;
+    user.name = name;
+    user.email = email;
+    user.password = password;
 
-        res.status(200).send("User updated!")
-    }
-}
+    res.status(200).send("User updated!");
+  }
+};
 
 const userDelete = (req, res) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    const indexUser = users.findIndex(user => user.id === Number(id) )
-    users.splice(indexUser, 1)
+  const indexUser = users.findIndex((user) => user.id === Number(id));
+  users.splice(indexUser, 1);
 
-    res.status(200).send("User deleted!")
-}
+  res.status(200).send("User deleted!");
+};
 
-module.exports = { 
-    usersList,
-    userGet,
-    userCreate,
-    userUpdate,
-    userDelete
-}
+module.exports = {
+  usersList,
+  userGet,
+  userCreate,
+  userUpdate,
+  userDelete,
+};
--- src/dump.sql
+++ src/dump.sql
@@ -1,11 +1,11 @@
 
 create database mercurio;
 
-create if not exists users(
+create table if not exists users(
 	id SERIAL primary key,
 	name VARCHAR(255),
-	email VARCHAR(255),
-	password VARCHAR(255)
+	email VARCHAR(255) unique,
+	password VARCHAR(255) 
 )
 
 INSERT INTO public.users(
