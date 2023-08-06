9ac7a975f8bc05bfc48f65a355cb557f96169214
Daniel <daniel.repres@hotmail.com>
2023-08-06 18:32:34 -0300

    Validators introductions

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,12 +1,8 @@
 const pool = require("../connection");
 
-const usersList = async (req, res) => {
+const usersList = async (_, res) => {
   try {
     const usersList = await pool.query("select * from users");
-
-    if (usersList.rowCount === 0) {
-      return res.status(404).send({ message: "Users not found" });
-    }
     return res.status(200).send(usersList.rows);
   } catch (error) {
     return res.status(500).send({ message: error.message });
@@ -23,7 +19,7 @@ const userGet = async (req, res) => {
     const userFound = await pool.query(query, params);
 
     if (userFound.rowCount === 0) {
-      return res.status(404).send({ message: "User not found" });
+      return res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).send(userFound.rows[0]);
@@ -33,33 +29,36 @@ const userGet = async (req, res) => {
 };
 
 const userCreate = async (req, res) => {
-  const data = req.body;
-
-  const newUser = {
-    name: data.name,
-    email: data.email,
-    password: data.password,
-  };
+  const { name, email, password } = req.body;
 
   try {
-    const queryEmail = "select * from users where email = $1";
-    const paramsEmail = [newUser.email];
-    const emailFound = await pool.query(queryEmail, paramsEmail);
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields must be filled" });
+    }
 
-    if (emailFound.rowCount > 0) {
-      return res.status(400).send({ message: "User already exists" });
+    const queryEmailExists = "select * from users where email = $1";
+    const paramsEmailExists = [email];
+
+    const emailAlreadyExists = await pool.query(
+      queryEmailExists,
+      paramsEmailExists
+    );
+
+    if (emailAlreadyExists.rowCount > 0) {
+      return res.status(400).json({ message: "Email already exists" });
     }
 
     const query =
-      "insert into users (name, email, password) values ($1, $2, $3)";
-    const params = [newUser.name, newUser.email, newUser.password];
-    const result = await pool.query(query, params);
+      "insert into users (name, email, password) values ($1, $2, $3) returning *";
+    const params = [name, email, password];
 
-    if (result.rowCount === 0) {
-      return res.status(400).send({ message: "User not created" });
+    const user = await pool.query(query, params);
+
+    if (user.rowCount === 0) {
+      return res.status(404).json({ message: "User not created" });
     }
 
-    res.status(201).send(newUser);
+    res.status(201).send(user.rows[0]);
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
@@ -70,33 +69,33 @@ const userUpdate = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    const query = "select * from users where id = $1";
-    const params = [Number(id)];
-    const userFound = await pool.query(query, params);
-
-    if (userFound.rowCount === 0) {
-      return res.status(404).send({ message: "User not found" });
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields must be filled" });
     }
 
-    const queryEmail = "select * from users where email = $1";
-    const paramsEmail = [email];
-    const emailFound = await pool.query(queryEmail, paramsEmail);
+    const queryEmailExists = "select * from users where email = $1";
+    const paramsEmailExists = [email];
 
-    if (emailFound.rowCount > 0) {
-      return res.status(400).send({ message: "User already exists" });
+    const emailAlreadyExists = await pool.query(
+      queryEmailExists,
+      paramsEmailExists
+    );
+
+    if (emailAlreadyExists.rowCount > 0) {
+      return res.status(400).json({ message: "Email already exists" });
     }
 
-    const queryUpdate =
-      "update users set name = $1, email = $2, password = $3 where id = $4";
-    const paramsUpdate = [name, email, password, Number(id)];
+    const query =
+      "update users set name = $1, email = $2, password = $3 where id = $4 returning *";
+    const params = [name, email, password, Number(id)];
 
-    const result = await pool.query(queryUpdate, paramsUpdate);
+    const userUpdated = await pool.query(query, params);
 
-    if (result.rowCount === 0) {
-      return res.status(400).send({ message: "User not updated" });
+    if (userUpdated.rowCount === 0) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({ message: "User updated!" });
+    res.status(200).send(userUpdated.rows[0]);
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
@@ -106,16 +105,15 @@ const userDelete = async (req, res) => {
   const { id } = req.params;
 
   try {
-    const query = "delete from users where id = $1";
+    const query = "delete from users where id = $1 returning *";
     const params = [Number(id)];
+    const userDeleted = await pool.query(query, params);
 
-    const result = await pool.query(query, params);
-
-    if (result.rowCount === 0) {
-      return res.status(404).send({ message: "User not found" });
+    if (userDeleted.rowCount === 0) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({ message: "User deleted!" });
+    res.status(200).send(userDeleted.rows[0]);
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
