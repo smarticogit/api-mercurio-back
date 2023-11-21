e4336c50e0b7755d44b1413c95cdd85b73ccacf9
Daniel <daniel.repres@hotmail.com>
2023-11-21 18:45:20 -0300

    feat: create login

--- src/controllers/user/user-get.js
+++ src/controllers/user/user-get.js
@@ -7,13 +7,15 @@ const userGet = async (req, res) => {
     const query = "select * from users where id = $1";
     const params = [Number(id)];
 
-    const userFound = await pool.query(query, params);
+    const { rowCount, rows } = await pool.query(query, params);
 
-    if (userFound.rowCount === 0) {
+    if (rowCount === 0) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    return res.status(200).send(userFound.rows[0]);
+    const { password: _, ...userData } = rows[0];
+
+    return res.status(200).send(userData);
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
